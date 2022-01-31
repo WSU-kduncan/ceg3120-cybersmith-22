e450efc4840d555464cfb6c846afb6855df73f9a
cybersmith-22 <smith.2362@wright.edu>
2022-01-31 14:35:09 +0000

    new branch, bot code modified and tested

--- Projects/Discord-Bot/bot.py
+++ Projects/Discord-Bot/bot.py
@@ -29,25 +29,57 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
+# jokes string array, containing the jokes
+    jokes = [
         (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
+            "A group of engineering students and their teacher were given free airplane tickets to go on a vacation"
+            "Once on the plane, the captain announced that they were on the plane the students had built. "
+            "Everyone freaked out and rushed out of the plane, except for the teacher who stayed there calmly. "
+	    "When the flight attendant asked why she hadn't left, she responded: "
+	    		"'I know the abilities of my students. This plane won't even start."
         ),
+	(
+            "My only wish upon dying is to have my remains scattered at Disneyland, that is unless they insist ."
+	    "on my remains being cremated. In that case just a regular burial."
+	),
+
+	(
+	    "If baseball really wanted to get exciting, they'd let a celebrity throw the LAST pitch. "
+	    "Bases loaded, here's Danny DeVito."
+	),
+            "Just got back from the mutation lab and boy are my arms legs.",
+
+        (
+	    "Me: It doesn't have a tail, so I'm pretty sure it's a hamster.\n"
+	    "Tech Support: Okay, fine. Right-click the hamster."
+	),
+	    "Guess who's got 7 thumbs and a set of keys to a cadaver lab?",
+	    "Gravitational waves create the heaviest surfing.",
+	(
+	    "Game: Choose your language\n"
+	    "Me: English\n"
+	    "Game: Are you sure? Language cannot be switched mid-game\n"
+            "Me [literally only speaks English but is terrified of commitment]: ...gimme a minute"
+	),
+	    "In the words of Mark Twain's wife, Shania Twain, 'that don't impress me much'"
     ]
 
-    hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+# memes string array, containing the path to the images
+    memes = [
+        "images/doomg.jpg",
+	"images/DebHee.jpg",
+	"images/busLegs.png",
+	"images/pakidge.jpg",
+	"images/DiscordTeacher.jpg"
     ]
 
-    if message.content == 'towel!':
-        #response = random.choice(brooklyn_99_quotes)
-        response = random.choice(hitchhiker_quotes)
-        await message.channel.send(response)
+# if statemnt comparing message content to possible command
+    if message.content == "!joke":
+        response = random.choice(jokes)
+       	await message.channel.send(response)
+    elif message.content == "!meme":
+        response = random.choice(memes)
+        await message.channel.send(file=discord.File(response))
+
 
 client.run(TOKEN)
